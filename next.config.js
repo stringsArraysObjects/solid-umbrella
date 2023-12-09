2eaa785ccab2b06864138b52d0cd21d3f60eeb3d
Chris <chtuff@gmail.com>
2023-12-09 00:29:35 -0500

    whatever

--- next.config.js
+++ next.config.js
@@ -1,24 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-
 }
 
 module.exports = nextConfig
-const isProd = process.env.NODE_ENV === 'production'
- 
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://remarkable-banoffee-90de4d.netlify.app/_next/static/chunks/4b9b41aaa062cbbfeff4add70f256968c51ece5d.4d708494b3aed70c04f0.js' : undefined,
-}
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/about',
-          destination: 'https://remarkable-banoffee-90de4d.netlify.app/',
-          permanent: true,
-        },
-      ]
-    },
-  }
+
+// const isProd = process.env.NODE_ENV === 'production'
+// module.exports = {
+//   // Use the CDN in production and localhost for development.
+//   assetPrefix: isProd ? 'https://remarkable-banoffee-90de4d.netlify.app/_next/static/chunks/4b9b41aaa062cbbfeff4add70f256968c51ece5d.4d708494b3aed70c04f0.js' : undefined,
+// }
+// module.exports = {
+//     async redirects() {
+//       return [
+//         {
+//           source: '/about',
+//           destination: 'https://remarkable-banoffee-90de4d.netlify.app/',
+//           permanent: true,
+//         },
+//       ]
+//     },
+//   }
