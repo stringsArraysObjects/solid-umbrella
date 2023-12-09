591e50afcbec5684c96f333eeba9dcc5efafbafa
Chris <chtuff@gmail.com>
2023-12-09 02:04:31 -0500

    I sux

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output:'export',
+
 }
 
 module.exports = nextConfig
@@ -9,15 +10,15 @@ module.exports = nextConfig
 // module.exports = {
 //   // Use the CDN in production and localhost for development.
 //   assetPrefix: isProd ? 'https://remarkable-banoffee-90de4d.netlify.app/_next/static/chunks/4b9b41aaa062cbbfeff4add70f256968c51ece5d.4d708494b3aed70c04f0.js' : undefined,
+//   async redirects() {
+//     return [
+//       {
+//         source: '/about',
+//         destination: 'https://remarkable-banoffee-90de4d.netlify.app/',
+//         permanent: true,
+//       },
+//     ]
+//   },
 // }
-// module.exports = {
-//     async redirects() {
-//       return [
-//         {
-//           source: '/about',
-//           destination: 'https://remarkable-banoffee-90de4d.netlify.app/',
-//           permanent: true,
-//         },
-//       ]
-//     },
-//   }
+
+  
