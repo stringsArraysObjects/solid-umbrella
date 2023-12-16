1ee8bc6dfae5ec75866288e91ec449fabe985f20
Chris <chtuff@gmail.com>
2023-12-15 22:18:58 -0500

    was able to get next.js to find the schema config file, by installing prisma again

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,21 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
 
-export const GET = () => {
-    return new NextResponse("Hello", {status: 200 });
+export const GET = async () => {
+    try{
+        const categories = await prisma.category.findMany()
+        return new NextResponse(
+            JSON.stringify ({categories}),
+            {status: 200}
+        );
+    }catch(err){
+     console.log(err);
+        return new NextResponse(
+            JSON.stringify ({ message: "Something went wrong!"}),
+            {status: 500}
+        );
+    }
 };
 export const POST = () => {
     return new NextResponse("Hello", {status: 200 });
