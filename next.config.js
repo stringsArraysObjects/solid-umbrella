4e0ac2c9765af2a32910240a98e8e4932054c114
Mason <128002915+grandpaCanCode@users.noreply.github.com>
2024-01-04 20:22:50 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
-   
-
-// }
-
-// module.exports = nextConfig
-
-// const isProd = process.env.NODE_ENV === 'production'
-// module.exports = {
-//   // Use the CDN in production and localhost for development.
-//   assetPrefix: isProd ? 'https://remarkable-banoffee-90de4d.netlify.app/_next/static/chunks/4b9b41aaa062cbbfeff4add70f256968c51ece5d.4d708494b3aed70c04f0.js' : undefined,
-//   async redirects() {
-//     return [
-//       {
-//         source: '/about',
-//         destination: 'https://remarkable-banoffee-90de4d.netlify.app/',
-//         permanent: true,
-//       },
-//     ]
-//   },
-// }
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
